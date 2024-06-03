1ba1fb961824606cfe54ad8fb45e2608531fa3ff
Josh Cooper <joshcooper@users.noreply.github.com>
2024-06-03 09:47:35 -0700

    Rename Amazon 7 to 2
    Amazon 7 is an implementation detail, the actual OS is named Amazon 2
    
    (cherry picked from commit 6bf00a03927893aec3a646a661eab79267021434)

--- configs/components/cpp-hocon.rb
+++ configs/components/cpp-hocon.rb
@@ -49,7 +49,7 @@ component 'cpp-hocon' do |pkg, settings, platform|
     toolchain = ''
     boost_static_flag = '-DBOOST_STATIC=OFF'
     special_flags = " -DENABLE_CXX_WERROR=OFF -DCMAKE_CXX_FLAGS='#{settings[:cflags]}'"
-    cmake = if platform.name =~ /amazon-7-aarch64/
+    cmake = if platform.name =~ /amazon-2-aarch64/
               '/usr/bin/cmake3'
             else
               'cmake'
